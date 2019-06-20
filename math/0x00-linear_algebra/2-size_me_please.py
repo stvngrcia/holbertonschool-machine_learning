75056acdf54080bedfb33d055e4d1939a54878a4
stvngrcia <stevengarciaacuna@gmail.com>
2019-06-20 04:20:11 +0000

    Adding more docs:

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""This file implements the a function to get the shape of a matrix"""
 def matrix_shape(matrix):
     """Gets the shape of a matrix
     :param matrix: The matrix for which this method is getting the shape
