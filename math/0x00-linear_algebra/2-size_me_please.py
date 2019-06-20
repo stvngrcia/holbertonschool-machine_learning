21efaa4ba704da408f21e9ef9b6f5186cf82a7ce
stvngrcia <stevengarciaacuna@gmail.com>
2019-06-20 04:15:18 +0000

    Adding docs

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
-    size = []
+    """Gets the shape of a matrix
+    :param matrix: The matrix for which this method is getting the shape
+    :return: A list of integers representing the shape of the matrix
+    """
+    shape = []
     while matrix:
-        size.append(len(matrix))
+        shape.append(len(matrix))
         matrix = matrix[0] if type(matrix[0]) == list else None
-    return size
+    return shape
