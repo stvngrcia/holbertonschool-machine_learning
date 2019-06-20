e1b0c38a773f26050e374efe2147fd891e842209
stvngrcia <stevengarciaacuna@gmail.com>
2019-06-20 05:39:39 +0000

    Transposing a 2D matrix

---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Implementation of a method to transpose a 2D matrix"""
+
+
+def matrix_transpose(matrix):
+    """Transposes a matrix
+    :param matrix: The given matrix to transpose
+    :return: A transpose matrix
+    """
+    new_matrix = []
+    flat_matrix = sum(matrix, [])
+    level_size = len(matrix[0])
+    start = 0
+    new_level = True
+    tracker = start
+    while True:
+        if tracker >= len(flat_matrix):
+            new_matrix.append(transpose)
+            new_level = True
+            start += 1
+        if new_level:
+            tracker = start
+            transpose = []
+            new_level = False
+        transpose.append(flat_matrix[tracker])
+        tracker += level_size
+
+        if start == level_size:
+            break
+    return new_matrix
