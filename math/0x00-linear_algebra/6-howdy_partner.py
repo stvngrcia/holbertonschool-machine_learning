3a0d8cce6dcc552421827d3168e007330e648ceb
steven garcia <sgarcia@pinterest.com>
2019-06-22 11:31:02 -0700

    Concatenating two arrays

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Implements array concatenation"""
+
+
+def cat_arrays(arr1, arr2):
+    """Concatenates two arrays
+    :param arr1: First array to concatenate
+    :param arr2: Second array to concatenate
+    :return: A concatenated array
+    """
+    return arr1 + arr2
